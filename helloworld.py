09acf92bffae93ef7a96f264835ed9c899b4d71f
Justine Agcanas <100750546+RhazeCoder@users.noreply.github.com>
2023-12-17 10:29:03 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,29 @@
-mport time
+import time
 
+hw = ""
 alpha = " abcdefghijklmnopqrstuvwxyz!"
 
 def process(index):
-    hw = "".join(alpha[i] for i in range(index))
-    result = []
-    for i in range(index):
-        result.append(hw[:i + 1])
+    global hw
+    i = 0
+    while i < index:
+        alphabet = list(alpha)
+        hw += alphabet[i]
+        print(hw)
+        if i != index - 1:
+            hw = hw[:-1]
+        i += 1
         time.sleep(0.1)
-    return result
 
-indices = [9, 6, 13, 13, 16, 1, 24, 16, 19, 13, 5, 28]
-
-for i in indices:
-    result = process(i)
-    print(result)
+process(9)
+process(6)
+process(13)
+process(13)
+process(16)
+process(1)
+process(24)
+process(16)
+process(19)
+process(13)
+process(5)
+process(28)
